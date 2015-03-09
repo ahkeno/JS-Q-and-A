d2afa26954464462ca60e9295108a88b04b70bb9
Chit <ahkeno@gmail.com>
2015-03-09 14:29:27 +0800

    Question 1: Coin Determiner

---
+++ Coin-Determiner.js
@@ -0,0 +1,14 @@
+function Coin-Determiner(num){
+	var count = 0;
+	do {
+		if (num = 15) num-= 15;
+		else if (num = 14) num -= 9;
+		else if (num >= 11) num -= 11;
+		else if (num >= 9) num -= 9;
+		else if (num >= 7) num -= 7;
+		else if (num >= 5) num -= 5;
+		else if (num >= 1) num -= 1;
+		count ++;
+	}while (num > 0);
+	return count;
+}
