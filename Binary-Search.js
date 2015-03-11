3ecc3b12e8eda138accf2489b1c6e8896455da26
Chit <ahkeno@gmail.com>
2015-03-11 15:06:06 +0800

    add Binary Seach for Binary Search Javascript apply Binary Search algorithm

---
+++ Binary-Search.js
@@ -0,0 +1,18 @@
+//Implement binary search
+var doSearch = function(array, targetValue) {
+	var min = 0;
+	var max = array.length - 1; 
+    var guess;
+    while (min <= max){
+      guess = (min + max)/2 | 0;
+      if (array[guess] === targetValue) { return guess;}
+      else if ( array [guess] < targetValue) {min = guess + 1;}
+      else {max = guess -1 ;}
+    }
+
+	return -1;
+};
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+var result = doSearch(primes, 73);
