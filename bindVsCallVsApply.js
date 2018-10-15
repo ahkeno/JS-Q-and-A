5915a64aa9341a81591ec838f70d18b696ddee6a
Chit Thiri Maung <ahkeno@gmail.com>
2018-10-15 17:04:21 +0800

    Update bindVsCallVsApply.js

--- bindVsCallVsApply.js
+++ bindVsCallVsApply.js
@@ -1,4 +1,7 @@
 //What is difference between blind() vs call () vs apply 
 
-
+//StackOverFlow
 https://stackoverflow.com/questions/15455009/javascript-call-apply-vs-bind
+
+//Medium Blog Post
+https://medium.com/@omergoldberg/javascript-call-apply-and-bind-e5c27301f7bb
